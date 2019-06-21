f554058f75f2fec5ff75f894fa06f62402acbaa7
Hudson, Philip <Philip.Hudson@zuhlke.com>
2019-06-21 14:25:23 +0100

    (config): creating security group is no longer required

--- screen-scraper-terraform/main.tf
+++ screen-scraper-terraform/main.tf
@@ -6,29 +6,31 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "screen-scrape-hsbc-terraform-state"
-    key    = "terraform/key"
+    key = "terraform/key"
     region = "eu-west-2"
     dynamodb_table = "screen-scrape-hsbc-terraform-state-dynamo"
   }
 }
 
 data "aws_security_group" "default" {
-  name   = "default"
+  name = "default"
   vpc_id = module.vpc.vpc_id
 }
 
 variable "azs" {
   description = "Run the EC2 Instances in these Availability Zones"
   type = "list"
-  default = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  default = [
+    "eu-west-2a",
+    "eu-west-2b",
+    "eu-west-2c"]
 }
 
 resource "aws_lb" "nlb" {
-  name               = "screen-scraper-nlb"
-  internal           = false
+  name = "screen-scraper-nlb"
+  internal = false
   load_balancer_type = "network"
-  subnets            = "${module.vpc.public_subnets}"
-  security_groups = ["${module.vpc.default_security_group_id}"]
+  subnets = "${module.vpc.public_subnets}"
 
   tags = {
     Name = "C24519-screen-scraper-hsbc-nlb"
@@ -42,18 +44,31 @@ module "vpc" {
 
   cidr = "10.32.0.0/16"
 
-  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
-  private_subnets = ["10.32.0.0/22", "10.32.4.0/22", "10.32.8.0/22","10.32.24.0/22", "10.32.28.0/22", "10.32.32.0/22"]
-  public_subnets  = ["10.32.12.0/22", "10.32.16.0/22", "10.32.20.0/22"]
+  azs = [
+    "eu-west-2a",
+    "eu-west-2b",
+    "eu-west-2c"]
+  private_subnets = [
+    "10.32.0.0/22",
+    "10.32.4.0/22",
+    "10.32.8.0/22",
+    "10.32.24.0/22",
+    "10.32.28.0/22",
+    "10.32.32.0/22"]
+  public_subnets = [
+    "10.32.12.0/22",
+    "10.32.16.0/22",
+    "10.32.20.0/22"]
 
   assign_generated_ipv6_cidr_block = true
 
-  enable_nat_gateway  = true
-  single_nat_gateway  = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   public_subnet_tags = {
     Name = "C24519-screen-scraper-hsbc-public"
   }
+
   private_subnet_tags = {
     Name = "C24519-screen-scraper-hsbc-private"
   }
@@ -70,10 +85,9 @@ module "vpc" {
 resource "aws_instance" "screen-scrape-ec2" {
   count = 3
 
-  ami           = "ami-07dc734dc14746eab"
+  ami = "ami-07dc734dc14746eab"
   instance_type = "t2.micro"
-  subnet_id     = "${module.vpc.private_subnets[count.index]}"
-  security_groups = ["${module.vpc.default_security_group_id}"]
+  subnet_id = "${module.vpc.private_subnets[count.index]}"
   availability_zone = "${element(var.azs, count.index)}"
 
   tags = {
