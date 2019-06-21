c2ac1202f6d97ba22576f80ac69d3c2fde9ee320
Hudson, Philip <Philip.Hudson@zuhlke.com>
2019-06-21 09:46:56 +0100

    initial commit.

---
+++ hsbc-terraform-aws/main.tf
@@ -0,0 +1,87 @@
+provider "aws" {
+  region = "eu-west-2"
+  profile = "default"
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
+
+
+
+resource "aws_lb" "nlb" {
+  name               = "screen-scraper-nlb"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = "${module.vpc.public_subnets}"
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name = "screen-scraper-hsbc-nlb"
+  }
+}
+
+module "vpc" {
+  source = "../"
+
+  name = "screen-scraper-vpc"
+
+  cidr = "10.32.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets = ["10.32.0.0/22", "10.32.4.0/22", "10.32.8.0/22","10.32.24.0/22", "10.32.28.0/22", "10.32.32.0/22"]
+  public_subnets  = ["10.32.12.0/22", "10.32.16.0/22", "10.32.20.0/22"]
+
+  assign_generated_ipv6_cidr_block = true
+
+  enable_nat_gateway  = true
+  single_nat_gateway  = true
+
+  public_subnet_tags = {
+    Name = "screen-scraper-hsbc-public"
+  }
+  private_subnet_tags = {
+    Name = "screen-scraper-hsbc-private"
+  }
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "screen-scraper-hsbc-poc"
+  }
+}
+
+resource "aws_instance" "ec2-2a" {
+  ami           = "ami-07dc734dc14746eab"
+  instance_type = "t2.micro"
+  subnet_id     = "${module.vpc.public_subnets[0]}"
+
+  tags = {
+    Name = "screen-scraper-hsbc-ec2-2a"
+  }
+}
+
+resource "aws_instance" "ec2-2b" {
+  ami           = "ami-07dc734dc14746eab"
+  instance_type = "t2.micro"
+  subnet_id     = "${module.vpc.public_subnets[1]}"
+
+  tags = {
+    Name = "screen-scraper-hsbc-ec2-2b"
+  }
+}
+
+resource "aws_instance" "ec2-2c" {
+  ami           = "ami-07dc734dc14746eab"
+  instance_type = "t2.micro"
+  subnet_id     = "${module.vpc.public_subnets[2]}"
+
+  tags = {
+    Name = "screen-scraper-hsbc-ec2-2c"
+  }
+}
